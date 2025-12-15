242771a11062b19ff2e0cc8b7e206c712e9f6f59
TechTucson <33633261+TechTucson@users.noreply.github.com>
2025-12-15 07:02:41 -0700

    Update and rename day1.py to day1_part1.py

--- AdventOfCode2025/Day1/day1_part1.py
+++ AdventOfCode2025/Day1/day1_part1.py
@@ -1,22 +1,26 @@
 import sys
 from collections import defaultdict, Counter
+#This will take your STDIN, easiest thing is to pipe it in with a command CAT INPUT.TXT | python AOCD12025.PY
 D = sys.stdin.read()
+#
 pos = 50
 print(pos)
 p1 = 0
 p2 = 0
 for line in D.splitlines():
+#This splits the first item of each line, which is used below if L or Else. If L is negative else +.
     d = line[0]
-	print(d)
+    print(d)
     amt = int(line[1:])
     for _ in range(amt):
         if d=='L':
             pos = (pos-1+100)%100
+            print(pos)
         else:
             pos = (pos+1)%100
+            print(pos)
         if pos==0:
             p2 += 1
     if pos==0:
         p1 += 1
 print(p1)
-print(p2)
