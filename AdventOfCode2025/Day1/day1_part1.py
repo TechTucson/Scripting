c736fb5d4b3b0d069403ab3c0f0b03a231f33bd9
TechTucson <33633261+TechTucson@users.noreply.github.com>
2025-12-16 05:29:48 -0700

    Improve movement logic in day1_part1.py
    Refactor movement logic and position tracking for better clarity.

--- AdventOfCode2025/Day1/day1_part1.py
+++ AdventOfCode2025/Day1/day1_part1.py
@@ -11,9 +11,11 @@ for line in D.splitlines():
 #This splits the first item of each line, which is used below if L or Else. If L is negative else +.
     d = line[0]
     print(d)
+#amt is the amount we are either moving up or down. 
     amt = int(line[1:])
     for _ in range(amt):
         if d=='L':
+#If this is negative, then we'll take the current position I think multiply it by -1 add 
             pos = (pos-1+100)%100
             print(pos)
         else:
@@ -21,6 +23,13 @@ for line in D.splitlines():
             print(pos)
         if pos==0:
             p2 += 1
+            print("P2")
+            print(p2)
     if pos==0:
         p1 += 1
+        print("P1")
+        print(p1)
+print("P1")
 print(p1)
+print("P2")
+print(p2)
